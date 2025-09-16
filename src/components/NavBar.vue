33853996aea2c187484773a3deb742cf444ac196
Lucas Costa <lucas.costa@simova.com.br>
2025-09-15 22:19:52 -0300

    feat: adiciona barra de navegação ao projeto

---
+++ src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- NavBar Mobile -->
+  <v-app-bar
+    flat
+    class="dark-bg"
+    v-if="isMobile"
+  >
+    <div class="ml-4 navbar-profile-img">Foto 3x4</div>
+    <v-spacer></v-spacer>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  </v-app-bar>
+  <!-- Drawer Mobile -->
+  <v-navigation-drawer
+    v-model="drawer"
+    class="dark-bg"
+    v-if="isMobile"
+  >
+    <v-list>
+      <v-list-item
+        v-for="item in menuItens" :key="item.id"
+      >
+        <v-list-item-title>{{ item.text }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <!-- Navbar Desktop -->
+  <v-navigation-drawer
+    v-else
+    class="dark-bg"
+  >
+    <v-list>
+      <v-list-item>Foto 3x4</v-list-item>
+      <v-divider></v-divider>
+      <v-list-item
+        v-for="item in menuItens" :key="item.id"
+      >
+        <v-list-item-title>{{ item.text }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+/**
+ * Barra de navegação (superior e lateral) da aplicação.
+ */
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      drawer: false,
+      windowWidth: window.innerWidth,
+      menuItens: [
+        { text: 'Início', id: 'home'},
+        { text: 'Sobre', id: 'about'},
+        { text: 'Projetos', id: 'projects'},
+        { text: 'Contato', id: 'contact'}
+      ]
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.windowWidth <= 960;
+    },
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.dark-bg {
+  background-color: #011518;
+  color: rgb(175, 175, 175);
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="min-h-screen w-full relative app-background">
+    <NavBar></NavBar>
+    <div>
+      <p>
+        Page body content
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique vehicula feugiat. Fusce rutrum gravida
+        libero, at maximus lorem ultrices non. Phasellus et nunc rhoncus, consectetur odio id, tristique magna. Sed
+        consequat accumsan magna eget aliquam. Fusce a consectetur erat. Morbi feugiat, mauris at porttitor ullamcorper,
+        nisl magna facilisis ante, eget sollicitudin arcu enim vel neque. Etiam nec diam orci. Ut ac massa fringilla nunc
+        mattis blandit. Aenean id velit quis est sodales congue nec vel nulla. Proin volutpat, ante et euismod cursus,
+        nunc massa maximus purus, sit amet auctor sapien leo eu lectus.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      //
+    }
+  },
+}
+</script>
+<style scoped>
+.app-background {
+  min-height: 100vh;
+  width: 100%;
+  position: relative;
+  background: #000;
+  color: white;
+}
+.app-background::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  z-index: 0;
+  background: radial-gradient(
+    ellipse 80% 60% at 50% 0%,
+    rgba(120, 180, 255, 0.25),
+    transparent 70%
+  );
+}
+
+/**
+ * Pequenos dispositivos
+ * Vuetify 'sm' Break Point
+ */
+@media (min-width: 600px) {
+
+}
+
+/**
+ * Dispositivos médios/grandes
+ * Vuetify 'md' Break Point
+ */
+@media (min-width: 960px) {
+
+}
+</style>
