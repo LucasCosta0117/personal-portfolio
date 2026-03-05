825b41066820c2aedc1a05f04aa293dc6155a72c
Lucas Costa <lucas.costa@simova.com.br>
2026-03-04 22:12:07 -0300

    feat: configura o i18n no 'main.js'

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import 'aos/dist/aos.css';
 import AOS from 'aos';
+import i18n from './i18n/i18n';
 
 loadFonts()
 AOS.init();
 
 createApp(App)
   .use(vuetify)
+  .use(i18n)
   .mount('#app')
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,12 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
+import i18n from '@/i18n/i18n'
+import { useI18n } from 'vue-i18n'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
+})
